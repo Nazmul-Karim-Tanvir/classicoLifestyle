6d191c74988842d4f7af0a7809b083dcfb7efb28
Nazmul Karim Tanvir <nazmulkarimtanvir@gmail.com>
2025-07-25 19:20:55 +0600

    changed it

--- classicoBackend/config/db.js
+++ classicoBackend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb+srv://nazmulkarimtanvir:123456789mojaTa@cluster0.sxgi6.mongodb.net/food-del');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
