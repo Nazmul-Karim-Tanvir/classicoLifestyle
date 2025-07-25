34aa8bee2c26c64ebf0c75ba5ed19dd1fc59ff8b
Nazmul Karim Tanvir <nazmulkarimtanvir@gmail.com>
2025-07-25 19:58:03 +0600

    changed in server.js

--- classicoBackend/server.js
+++ classicoBackend/server.js
@@ -7,11 +7,12 @@ import productRoutes from './routes/productRoutes.js';
 dotenv.config();
 connectDB();
 
+// app config
 const app = express();
+
+// middleware
 app.use(cors());
 app.use(express.json());
-
-app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
   res.send('Classico API Running');
