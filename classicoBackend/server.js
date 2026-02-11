b203dd7a08844d2445698d9724b5641c1dca54d6
Nazmul-karim-tanvir-2nd <140707884+Nazmul-karim-tanvir-2nd@users.noreply.github.com>
2026-02-11 22:51:49 +0600

    working on auth api

--- classicoBackend/server.js
+++ classicoBackend/server.js
@@ -1,33 +1,41 @@
 // F:\classico lifestyle\classicoLifestyle\classicoBackend\server.js
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import productRouter from './routes/productRoutes.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import productRouter from "./routes/productRoutes.js";
+import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
 
-// app config
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// db connection
-connectDB();
+// static folder for images
+app.use("/images", express.static("uploads"));
 
-// api endpoints
-app.use('/api/product', productRouter);
-app.use('/images',express.static('uploads'));
+// routes
+app.use("/api/product", productRouter);
+app.use("/api/auth", authRouter);
 
-app.get('/', (req, res) => {
-  res.send('Classico LifeStyle API Running');
+// root
+app.get("/", (req, res) => {
+  res.send("Classico LifeStyle API Running");
+});
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 // mongodb+srv://classicocode:124365789mojaTa@cluster0.bwipms0.mongodb.net/?
 
